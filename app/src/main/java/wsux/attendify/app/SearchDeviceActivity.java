e96a46fc0a9d0a6ed2db1419760e1adc00d9e369
Omry Rosenberg <omryro@cs.huji.ac.il>
2018-10-22 15:30:17 +0300

    added base files

---
+++ app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
@@ -0,0 +1,13 @@
+package wsux.attendify.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchDeviceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_device);
+    }
+}
