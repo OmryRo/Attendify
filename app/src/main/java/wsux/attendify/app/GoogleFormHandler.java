702b59bc124609bf7671c7125cb82e226566a521
Omry Rosenberg <omry99@gmail.com>
2018-10-25 10:27:10 +0300

    GoogleFormHandler now works, implemented the validate function and added some attributes to the post so google won't ignore us as a bot.

--- app/src/main/java/wsux/attendify/app/GoogleFormHandler.java
+++ app/src/main/java/wsux/attendify/app/GoogleFormHandler.java
@@ -13,6 +13,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -24,88 +25,68 @@ import java.util.StringJoiner;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class GoogleFormHandler {
 
     private static final String TAG = "GOOGLE_FORM_HANDLER";
-    private static final String FORM_URL =
-            "https://docs.google.com/forms/d/e/1FAIpQLSeaKBHeSVGBpQZeUv2VxMDjxfcEtZ0vfobn99UKDqaX_g7ICg/viewform";
+    private static final String FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeaKBHeSVGBpQZeUv2VxMDjxfcEtZ0vfobn99UKDqaX_g7ICg/";
+    private static final String FORM_URL_GET = String.format("%sviewform", FORM_URL);
+    private static final String FORM_URL_POST = String.format("%sformResponse", FORM_URL);
     private static final String FORM_REAL_NAME_INPUT_ID = "entry.90001332";
     private static final String FORM_EMAIL_INPUT_ID = "entry.84085877";
     private static final String FORM_DEVICE_ID_INPUT_ID = "entry.2012310578";
+    private static final int HTTP_TIMEOUT = 10000;
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36";
 
     public static void sendGoogleForm(String name, String email, long deviceId, OnGoogleFormResponse onGoogleFormResponse) {
         new SendGoogleForm().execute(name, email, String.valueOf(deviceId), onGoogleFormResponse);
     }
 
     private static Pair<Integer, String> requestGoogleForm(String formURL, Map<String, String> postParams) {
-        int responseCode;
-        StringBuffer response = new StringBuffer();
 
-        try {
-            URL url = new URL(FORM_URL);
-            URLConnection connection = url.openConnection();
-            HttpsURLConnection https = (HttpsURLConnection) connection;
-
-            if (postParams != null) {
-                https.setRequestMethod("POST");
-                https.setDoOutput(true);
-
-                StringBuilder sb = new StringBuilder();
-                for (Map.Entry<String, String> entry : postParams.entrySet())
-                    sb.append(String.format(
-                            "%s=%s&",
-                            URLEncoder.encode(entry.getKey(), "UTF-8"),
-                            URLEncoder.encode(entry.getValue(), "UTF-8")
-                    ));
-                sb.deleteCharAt(sb.length() - 1);
-                byte[] out = sb.toString().getBytes(StandardCharsets.UTF_8);
-                int length = out.length;
-
-                https.setFixedLengthStreamingMode(length);
-                https.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-                https.connect();
-
-                OutputStream os = https.getOutputStream();
-                os.write(out);
-                os.flush();
-                os.close();
-            } else {
-                https.setRequestMethod("GET");
-            }
-
-            responseCode = https.getResponseCode();
-
-            if (responseCode != HttpsURLConnection.HTTP_OK) {
-                throw new IOException(String.format("Bad http response %s", responseCode));
-            }
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            Log.d(TAG, String.format("sendGoogleForm: %s", response));
-
-        } catch (IOException e) {
-            Log.e(TAG, "sendGoogleForm: ", e);
-            return null;
+        OkHttpClient client = new OkHttpClient();
+        FormBody.Builder formBodyBuilder = new FormBody.Builder();
+        for (Map.Entry<String, String> params : postParams.entrySet()) {
+            formBodyBuilder.add(params.getKey(), params.getValue());
         }
 
-        return new Pair<>(responseCode, response.toString());
+        FormBody body = formBodyBuilder.build();
+
+        Request request = new Request.Builder()
+                .url(formURL)
+                .header("User-Agent", USER_AGENT)
+                .header("Upgrade-Insecure-Requests", "1")
+                .header("Accept-Charset", "UTF-8")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .post(body)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return new Pair<>(response.code(), response.body().string());
+        } catch (IOException e) {
+            Log.e(TAG, "requestGoogleForm: ", e);
+        }
+
+        return null;
     }
 
     private static Map<String, String> getAntiBottingInfo(String formURL) {
 
-        Pair<Integer, String> response = requestGoogleForm(formURL, null);
-
-        if (response == null) {
+        Document googleForm;
+        try {
+            URL url = new URL(formURL);
+            googleForm = Jsoup.parse(url, HTTP_TIMEOUT);
+        } catch (IOException e) {
+            Log.e(TAG, "getAntiBottingInfo: ", e);
             return null;
         }
 
-        Document googleForm = Jsoup.parse(response.second);
+        // anti logging form fields from the html, if there are  more, add them here.
         Elements fieldsFvv = googleForm.getElementsByAttributeValue("name", "fvv");
         Elements fieldsDraftResponse = googleForm.getElementsByAttributeValue("name", "draftResponse");
         Elements fieldsPageHistory = googleForm.getElementsByAttributeValue("name", "pageHistory");
@@ -116,10 +97,23 @@ public class GoogleFormHandler {
         botTraps.put("draftResponse", fieldsDraftResponse.attr("value"));
         botTraps.put("pageHistory", fieldsPageHistory.attr("value"));
         botTraps.put("fbzx", fieldsFbzx.attr("value"));
+
         return botTraps;
     }
 
     private static boolean validateResponse(String output, int responseCode) {
+        if (responseCode != HttpURLConnection.HTTP_OK) {
+            Log.d(TAG, "validateResponse: failed, response code: " + responseCode);
+            return false;
+        }
+
+        Document response = Jsoup.parse(output);
+        Elements confirmationElements = response.getElementsByClass("freebirdFormviewerViewResponseConfirmContentContainer");
+        if (confirmationElements.size() == 0) {
+            Log.d(TAG, "validateResponse: failed, no confirmation message: " + output);
+            return false;
+        }
+
         return true;
     }
 
@@ -127,7 +121,7 @@ public class GoogleFormHandler {
 
         @Override
         protected Pair<OnGoogleFormResponse, Boolean> doInBackground(Object... objects) {
-            Map<String, String> params = getAntiBottingInfo(FORM_URL);
+            Map<String, String> params = getAntiBottingInfo(FORM_URL_GET);
 
             if (params == null) {
                 return new Pair<>((OnGoogleFormResponse) objects[3], false);
@@ -137,7 +131,7 @@ public class GoogleFormHandler {
             params.put(FORM_EMAIL_INPUT_ID, (String) objects[1]);
             params.put(FORM_DEVICE_ID_INPUT_ID, (String) objects[2]);
 
-            Pair<Integer, String> response = requestGoogleForm(FORM_URL, params);
+            Pair<Integer, String> response = requestGoogleForm(FORM_URL_POST, params);
             boolean success =  response != null && validateResponse(response.second, response.first);
             return new Pair<>((OnGoogleFormResponse) objects[3], success);
         }
