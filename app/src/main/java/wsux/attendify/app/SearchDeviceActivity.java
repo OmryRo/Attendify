2bfad2528501124ce2bc2515b9c60987a4e09fae
Abuhay Abune <abuhay.abune@cs.huji.ac.il>
2018-10-26 18:15:59 +0300

    add content_main.xml

--- app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
+++ app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
@@ -5,7 +5,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -27,7 +29,35 @@ import com.google.android.gms.nearby.messages.MessagesOptions;
 import com.google.android.gms.nearby.messages.NearbyPermissions;
 
 import android.support.design.widget.Snackbar;
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.service.carrier.CarrierMessagingService;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.messages.MessageFilter;
+import com.google.android.gms.nearby.messages.MessagesOptions;
+import com.google.android.gms.nearby.messages.NearbyMessagesStatusCodes;
+import com.google.android.gms.nearby.messages.NearbyPermissions;
+import com.google.android.gms.nearby.messages.Strategy;
+import com.google.android.gms.nearby.messages.SubscribeOptions;
 
 public class SearchDeviceActivity extends FragmentActivity
     implements GoogleApiClient.ConnectionCallbacks,
@@ -51,7 +81,7 @@ public class SearchDeviceActivity extends FragmentActivity
 
 //        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(myToolbar);
-
+        mContainer = (RelativeLayout) findViewById(R.id.main_activity_container);
         if(savedInstanceState != null)
         {
             mSubscribe = savedInstanceState.getBoolean(KEY_SUBSCRIBED,false);
@@ -91,7 +121,7 @@ public class SearchDeviceActivity extends FragmentActivity
     public void onSaveInstanceState(Bundle outState)
     {
         super.onSaveInstanceState(outState);
-
+        outState.putBoolean(KEY_SUBSCRIBED,mSubscribe);
     }
 
     private boolean havePermissions()
@@ -123,7 +153,12 @@ public class SearchDeviceActivity extends FragmentActivity
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
     {
-
+        if (mContainer != null) {
+            Snackbar.make(mContainer,
+                    "Exception while connecting to Google Play services: " +
+                            connectionResult.getErrorMessage(),
+                    Snackbar.LENGTH_INDEFINITE).show();
+        }
     }
     private synchronized void buildGoogleApiClient() {
         if (mGoogleApiClient == null) {
@@ -160,5 +195,45 @@ public class SearchDeviceActivity extends FragmentActivity
 
             }
         });
+    }
+
+
+    private void showLinkToSettingsSnackbar() {
+        if (beaconHandler == null) {
+            return;
+        }
+        Snackbar.make(mContainer,
+                R.string.permission_denied_explanation,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.settings, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Build intent that displays the App settings screen.
+                        Intent intent = new Intent();
+                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package",
+                                BuildConfig.APPLICATION_ID, null);
+                        intent.setData(uri);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                }).show();
+    }
+
+    private void showRequestPermissionsSnackbar() {
+        if (mContainer == null) {
+            return;
+        }
+        Snackbar.make(mContainer, R.string.permission_rationale,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Request permission.
+                        ActivityCompat.requestPermissions(SearchDeviceActivity.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                PERMISSIONS_REQUEST_CODE);
+                    }
+                }).show();
     }
 }
