eb277bd35168853e773dba44fe4d3ad03388c5ef
abuhay.abune <T2c5k_l8sB>
2018-10-29 17:06:13 +0200

    now will close on deny starting bluetoothe

--- app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
+++ app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
@@ -77,4 +77,12 @@ public class SearchDeviceActivity extends Activity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == BeaconHandler.REQUEST_ENABLE_BT && resultCode == RESULT_CANCELED) {
+            finish();
+        }
+    }
 }
