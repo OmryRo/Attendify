54e1a1e1ad16d85aa14dcb672d9514314232b639
abuhay.abune <T2c5k_l8sB>
2018-10-29 16:43:52 +0200

    revert to the file SearchDeviceActivity

--- app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
+++ app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
@@ -1,17 +1,27 @@
 package wsux.attendify.app;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class SearchDeviceActivity extends Activity {
 
     private BeaconHandler beaconHandler;
+    private static final int FINE_LOCATION_REQUEST_CODE = 1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_device);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+            }
+        }
 
         beaconHandler = new BeaconHandler(this, new BeaconHandler.OnBeaconSearchResult() {
             @Override
@@ -49,4 +59,22 @@ public class SearchDeviceActivity extends Activity {
         beaconHandler.stop();
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case FINE_LOCATION_REQUEST_CODE: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+
 }
