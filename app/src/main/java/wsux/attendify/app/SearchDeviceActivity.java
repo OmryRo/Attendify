fcfe7eaed200eaecd4ce47c27b2dcf7de46322af
haimofer <ofer.haim@mail.huji.ac.il>
2018-11-02 16:26:00 +0200

    Added spinner screen

--- app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
+++ app/src/main/java/wsux/attendify/app/SearchDeviceActivity.java
@@ -2,16 +2,32 @@ package wsux.attendify.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SearchDeviceActivity extends Activity {
 
     private BeaconHandler beaconHandler;
+    ImageView loadingIV;
+    TextView loadingTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_device);
+        loadingTV = (TextView) findViewById(R.id.loadingTV);
+        loadingIV = (ImageView) findViewById(R.id.loadingIV);
+
+        Animation animation = new RotateAnimation(0.0f, 360.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        animation.setRepeatCount(-1);
+        animation.setDuration(2000);
+        loadingIV.setAnimation(animation);
 
         beaconHandler = new BeaconHandler(this, new BeaconHandler.OnBeaconSearchResult() {
             @Override
@@ -32,6 +48,10 @@ public class SearchDeviceActivity extends Activity {
 
             @Override
             public void notFound(int numberOfOtherDevicesInScan) {
+                loadingIV.clearAnimation();
+
+                loadingTV.setText(getString(R.string.devices_found, numberOfOtherDevicesInScan));
+
 
             }
         });
