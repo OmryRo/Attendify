7c3de2488b5bca57db2c51b062257092087aeba7
haimofer <ofer.haim@mail.huji.ac.il>
2018-11-11 20:53:09 +0200

    trimmed the inputs

--- app/src/main/java/wsux/attendify/app/MainActivity.java
+++ app/src/main/java/wsux/attendify/app/MainActivity.java
@@ -92,8 +92,8 @@ public class MainActivity extends Activity {
                     return;
                 }
 
-                final String email = emailET.getText().toString();
-                final String name = nameET.getText().toString();
+                final String email = emailET.getText().toString().trim();
+                final String name = nameET.getText().toString().trim();
 
                 if (!name.matches(NAME_MATCH)) {
                     popup(R.string.invalid_name);
